f7972c1c238c9e52aa72a4edf4eb66fd8264547c
DallinAtnip <46285965+DallinAtnip@users.noreply.github.com>
2019-05-31 12:32:25 -0500

    first commit
    the base of the program

---
+++ characterinput.py
@@ -0,0 +1,12 @@
+#user gives their age then we tell them how long it will take for them to reach 100
+#ask user for another number and the program wil print off the message that many times
+#print out each message on a seperate line
+#Dallin Atnip 05/30/19
+
+age = int(input("how old are you?: "))
+
+agedifference = 100 - age
+
+print "you will be 100 in",agedifference,"years"
+
+usernumber = int(input("pick a number: "))
